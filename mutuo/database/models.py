49f8dacc812fad6fe33688a9d2ef9ad16f416565
BrenDev0 <lahey1991@gmail.com>
2026-03-19 15:57:27 -0600

    user table

--- mutuo/database/models.py
+++ mutuo/database/models.py
@@ -1,4 +1,10 @@
+from datetime import datetime, timezone
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import DateTime
+from sqlalchemy.orm import mapped_column, Mapped
 
 class Base(DeclarativeBase):
     pass
+
+class TimeStampMixin:
+    created_at: Mapped[DateTime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
--- mutuo/users/models.py
+++ mutuo/users/models.py
@@ -1,7 +1,21 @@
-from mutuo.database.models import Base
+import uuid
+from datetime import datetime
+from sqlalchemy import String, DateTime, func
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.dialects.postgresql import UUID
+
+from mutuo.database.models import Base, TimeStampMixin
 
 
-class User(Base):
+class User(Base, TimeStampMixin):
     __tablename__ = "users"
 
-    
+    user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False)
+    email_hash: Mapped[str] = mapped_column(String, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    profile_type: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+
