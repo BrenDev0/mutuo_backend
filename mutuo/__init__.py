9eee4f99a2f3db7bf2d24c8270880ae27e517201
Brendan Lahey <lahey1991@gmail.com>
2026-03-20 21:28:43 -0600

    create tables in db

---
+++ mutuo/__init__.py
@@ -0,0 +1,1 @@
+from mutuo.users.models import User
--- mutuo/database/core.py
+++ mutuo/database/core.py
@@ -1,6 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 from mutuo.settings import settings
 
+
 engine = create_async_engine(
     url=settings.DATABASE_URL
 )
+
+
