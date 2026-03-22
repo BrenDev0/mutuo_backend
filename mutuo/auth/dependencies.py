3231b30013a2383d7ad2ca2782622baf6852a70a
Brendan Lahey <lahey1991@gmail.com>
2026-03-21 19:09:10 -0600

    user auth

---
+++ mutuo/auth/dependencies.py
@@ -0,0 +1,62 @@
+from fastapi import Request, HTTPException, Depends
+from uuid import UUID
+
+from mutuo.cache.protocols import CacheStore
+from mutuo.security.encryption import decrypt
+from mutuo.users.service import get_by_id
+from mutuo.users.transformers import to_user_public
+
+from .schemas import SessionSchema
+
+
+def get_session_id(
+    request: Request
+):
+    session_id = request.cookies.get("session_id")
+
+    if not session_id:
+        raise HTTPException(status_code=400, detail="Unauthorized")
+    
+    return UUID(session_id)
+
+
+async def get_current_user(
+    request: Request,
+    session_id: UUID = Depends(get_session_id)
+):
+    cache_store: CacheStore = request.app.state.cache_store
+    session = await cache_store.get(str(session_id))
+
+    if session is None:
+        raise HTTPException(status_code=400, detail="Unauthorized")
+    
+    session = SessionSchema(**session)
+    
+    user_cache_key = f"chache:user:{session.user_id}"
+    user_cache = await cache_store.get(user_cache_key)
+
+    if not user_cache:
+        user = await get_by_id(
+            db=request.state.db,
+            user_id=session.user_id
+        )
+
+        if not user:
+            raise HTTPException(status_code=400, detail="Unauthorized")
+        
+        user_public = to_user_public(
+            user=user,
+            decryption=decrypt
+        )
+
+        await cache_store.set(
+            key=user_cache_key,
+            value=user_public.model_dump(mode="json"),
+            expire_seconds=60*5
+        )
+
+    return user_public
+
+
+        
+
--- mutuo/auth/usecases.py
+++ mutuo/auth/usecases.py
@@ -3,7 +3,7 @@ from typing import Callable, Awaitable
 from uuid import UUID, uuid4
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from mutuo.utils import utc_now_iso
+from mutuo.utils import utc_now
 from mutuo.settings import settings
 from mutuo.exceptions import UnauthorizedException
 from mutuo.security.hashing import DeterministicHashFn, CompareHashFn
@@ -14,7 +14,7 @@ from mutuo.users.models import User
 from mutuo.users.schemas import UserPublic
 from mutuo.users.transformers import to_user_public
 
-from .schemas import LoginCredentials, SessionContext
+from .schemas import LoginCredentials, SessionContext, SessionSchema
 
 
 GetByEmailHashFn = Callable[[AsyncSession, str], Awaitable[User | None]]
@@ -26,19 +26,19 @@ async def create_session(
 ) -> UUID:
     session_id = uuid4()
 
-    session_cache = {
-        "user_id": str(user.user_id),
-        "ip": session_context.ip,
-        "client_agent": session_context.client_agent,
-        "created_at": utc_now_iso()
-    }
+    session_cache = SessionSchema(
+        user_id=user.user_id,
+        ip=session_context.ip,
+        client_agent=session_context.client_agent,
+        created_at=utc_now()
+    )
 
     user_cahe = user.model_dump(mode="json")
 
     await asyncio.gather(
         cache_store.set(
             key=f"{str(session_id)}",
-            value=session_cache,
+            value=session_cache.model_dump(mode="json"),
             expire_seconds=settings.SESSION_MAX_AGE
         ),
         cache_store.set(
@@ -49,6 +49,16 @@ async def create_session(
     )
 
     return session_id
+
+async def delete_session(
+    cache_store: CacheStore,
+    session_id: UUID,
+    user_id: UUID
+):
+    await asyncio.gather(
+        cache_store.delete(str(session_id)),
+        cache_store.delete(f"cache:user{user_id}")
+    )
 
 
 
