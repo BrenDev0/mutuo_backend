5cbeafb0d8fdba1a05c82dc6800e8a7ed90f7106
BrenDev0 <lahey1991@gmail.com>
2026-04-30 15:57:01 -0600

    contracts router mount to api

--- mutuo/app/api.py
+++ mutuo/app/api.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from mutuo.users.routes import router as users_router
 from mutuo.auth.routes import router as auth_router
 from mutuo.listings.routes import router as listings_router
+from mutuo.contracts.routes import router as contracts_router
 
 router = APIRouter()
 
@@ -10,3 +11,4 @@ router = APIRouter()
 router.include_router(auth_router, prefix="/auth")
 router.include_router(users_router, prefix="/users")
 router.include_router(listings_router, prefix="/listings")
+router.include_router(contracts_router, prefix="/contracts")
