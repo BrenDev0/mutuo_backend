1b8c4a05086d2dd9085e47ab11a00047b28d9ebd
BrenDev0 <lahey1991@gmail.com>
2026-05-14 10:31:05 -0600

    state atribute getter method bug

--- mutuo/cache/dependencies.py
+++ mutuo/cache/dependencies.py
@@ -2,7 +2,7 @@ from fastapi import Request
 from .protocols import CacheStore
 
 def get_cache_store(request: Request) -> CacheStore:
-    cache_store = request.app.state.get("cache_store")
+    cache_store = getattr(request.app.state, "cache_store", None)
     if not cache_store:
         raise ValueError("Cache store not initialized in app")
     
